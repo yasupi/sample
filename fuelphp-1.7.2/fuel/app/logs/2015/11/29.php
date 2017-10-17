8654470759ef35a0b68cc23a2319ec176bcf3297
yasuyasu <dev.niwayasu@gmail.com>
2017-10-17 22:45:07 +0900

    サンプル公開に必要ないものを削除

--- fuelphp-1.7.2/fuel/app/logs/2015/11/29.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2015-11-29 04:24:33 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2015-11-29 04:24:33 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
